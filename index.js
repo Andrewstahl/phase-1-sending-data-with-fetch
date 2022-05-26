8a44ead01d0ff2f19b13cb1770589ae9e4b2d5db
Andrew Stahl <andrewstahl96@gmail.com>
2022-05-26 05:52:59 -0400

    commit tree 3b492925d1108c15a0d65e3d45b05690a9696fe1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+function submitData(name, email) {
+  const fetchReturn = fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email
+    })
+  })
+  .then(res => res.json())
+  // .then(data => console.log(data['name']))
+  .then(data => {
+    const body = document.querySelector("body");
+    body.innerHTML = `${data.id}`;
+    })
+  .catch(error => {
+    alert("Error Found!");
+    const body = document.querySelector("body");
+    body.innerHTML = `${error.message}`;
+    })
+  return fetchReturn;
+}
+
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
